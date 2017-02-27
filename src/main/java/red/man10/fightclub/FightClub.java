89c78ae47152884d1a768371b1a5a068ca0d2628
Takashi Ohtsuka <takatronix@gmail.com>
2017-02-28 07:29:22 +0900

    Initial commit

---
+++ src/main/java/red/man10/fightclub/FightClub.java
@@ -0,0 +1,17 @@
+package red.man10.fightclub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FightClub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
