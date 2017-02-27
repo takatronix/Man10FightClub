4949e401115ce16d5b3386c49c5ad425ebd32d94
Takashi Ohtsuka <takatronix@gmail.com>
2017-02-28 08:13:13 +0900

    enum Status 追加

--- src/main/java/red/man10/fightclub/FightClub.java
+++ src/main/java/red/man10/fightclub/FightClub.java
@@ -4,6 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FightClub extends JavaPlugin {
 
+    //   状態遷移
+    public enum Status {
+        Closed,                 //  開催前
+        Opened,                 //  募集中 予想の受付開始
+        Playing,                //  対戦中
+    }
+
     @Override
     public void onEnable() {
         // Plugin startup logic
