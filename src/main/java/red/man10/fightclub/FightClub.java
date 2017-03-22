b8cfefde7f713b35e13b5f1d4b989d80614e89ea
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-22 14:20:02 +0900

    戦闘中にコマンドを選手以外もつかえなくした

--- src/main/java/red/man10/fightclub/FightClub.java
+++ src/main/java/red/man10/fightclub/FightClub.java
@@ -1622,15 +1622,13 @@ public final class FightClub extends JavaPlugin implements Listener {
     public void commandCancel(PlayerCommandPreprocessEvent e){
         if(currentStatus == Status.Fighting) {
             Player p = e.getPlayer();
-            if (p.getWorld().getName().equalsIgnoreCase("arena")) {
-                if (p.getName().equalsIgnoreCase(fighters.get(0).name) || p.getName().equalsIgnoreCase(fighters.get(1).name)) {
+            if (p.getWorld().getName().equalsIgnoreCase(worldName)) {
                     if(p.isOp()){
                         return;
                     }
                     p.sendMessage(prefix + "戦闘中はコマンドの使用はできません");
                     e.setCancelled(true);
                     return;
-                }
             }
         }
     }
